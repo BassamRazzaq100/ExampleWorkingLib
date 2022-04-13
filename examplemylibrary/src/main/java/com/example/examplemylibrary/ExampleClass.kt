37fb9f88862de69be1a94b3888daf6e599a77d5c
Bassam <bassam.razzaq@arhamsoft.com>
2022-04-13 12:13:29 +0500

    First Commit

---
+++ examplemylibrary/src/main/java/com/example/examplemylibrary/ExampleClass.kt
@@ -0,0 +1,11 @@
+package com.example.examplemylibrary
+
+import android.content.Context
+import android.widget.Toast
+
+class ExampleClass {
+
+    fun s(c: Context?, message: String?) {
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+    }
+}
