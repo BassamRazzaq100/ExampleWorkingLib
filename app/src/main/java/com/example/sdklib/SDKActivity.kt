debfcf2f13693700360b5f83e54292c7e689144a
Bassam <bassam.razzaq@arhamsoft.com>
2022-04-14 10:49:24 +0500

    2nd commit

--- app/src/main/java/com/example/sdklib/SDKActivity.kt
+++ app/src/main/java/com/example/sdklib/SDKActivity.kt
@@ -3,9 +3,10 @@ package com.example.sdklib
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SDKActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
--- examplemylibrary/src/main/java/com/example/examplemylibrary/ExampleClass.kt
+++ examplemylibrary/src/main/java/com/example/examplemylibrary/ExampleClass.kt
@@ -8,4 +8,5 @@ class ExampleClass {
     fun s(c: Context?, message: String?) {
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
     }
+
 }
