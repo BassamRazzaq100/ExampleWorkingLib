d09fe83a7ae7de352c9d00a577843daa73747fcd
Bassam <bassam.razzaq@arhamsoft.com>
2022-04-14 11:36:55 +0500

    Bug Fix

--- app/src/main/java/com/example/sdklib/SDKActivity.kt
+++ app/src/main/java/com/example/sdklib/SDKActivity.kt
@@ -3,7 +3,7 @@ package com.example.sdklib
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class SDKActivity : AppCompatActivity() {
+open class SDKActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
